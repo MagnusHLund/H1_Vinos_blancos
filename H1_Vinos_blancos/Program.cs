80567b6419e9ceca878d04c3bb0724971971a166
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2023-08-15 18:26:06 +0200

    Works, using normal array

--- H1_Vinos_blancos/Program.cs
+++ H1_Vinos_blancos/Program.cs
@@ -21,98 +21,62 @@
         static void Controller()
         {
             // Creates a new array, which gets the data from the Model() method
-            int[,] data = Model();
+            int[] data = Model();
 
-            int[] sales = new int[11];
-            int[] backup = new int[11];
-
-            for (int i = 0; i < data.Length / 2; i++)
-            {
-                int is2014 = (int)data.GetValue(i, 0);
-
-                if (is2014 == 2014)
-                {
-                    data[i, 1] = data[i, 1] + 35432;
-                }
-
-                sales[i] = (int)data.GetValue(i, 1);
-                backup[i] = sales[i];
-            }
-
-            Array.Sort(sales);
-
-            for (int i = 0; i < data.Length; i++)
-            {
-
-                int position = Array.IndexOf(sales, data[i, 1]);
-
-                bool contains = sales.Contains(data[position, 1]);
-
-                if (contains)
-                {
-                    sales[i] = data[i, 1];
-
-                    for (int j = 0; j < data.Length; j++)
-                    {
-                        int dataCheck = data[j, 1];
-                        int modelCheck = Model()[i, 1];
-                        if (data[j, 1] == Model()[i, 1])
-                        {
-                            data[j, 0] = Model()[i, 0];
-                        }
-                        else
-                        {
-                            data[j, 0] = 2014;
-                        }
-                    }
-                }
-
-                //Console.WriteLine(sales.Contains(data[position, 1]));
-                Console.WriteLine(sales[i]);
-                Console.WriteLine(data[position, 1]);
-            }
-
-
+            // Creates a bool which should only run once
+            bool doOnce = false;
 
             // Sets a const value of max and maxStars
             const int max = 175388;
             const byte maxStars = 100;
 
-            // 
-            for (int i = 0; i < data.Length / 2; i++)
+            // Runs a for loop, as many times as there is ints in data
+            for (int i = 0; i < data.Length; i++)
             {
-
-                int star = maxStars * data[i, 1] / max;
-
-                //Console.Write($"{data[i, 0]}: ");
-
-                for (int j = 0; j < star; j++)
+                // Do this once
+                if (!doOnce)
                 {
-                    // View();
+                    // Changes the 5th int (2014), by adding 35432 to the value and changes the 
+                    data[5] = data[5] + 35432;
+                    doOnce = !doOnce;
                 }
 
+                // Sorts the data, by highest and lowest value
+                Array.Sort(data);
+
+                // Calculates how many stars should be outputted
+                int star = maxStars * data[i] / max;
+
+                // Calls the View() method, until j is as big as star
+                for (int j = 0; j < star; j++)
+                {
+                    View();
+                }
+
+                // Goes to the next line
                 Console.WriteLine();
             }
         }
 
-        static int[,] Model()
+        static int[] Model()
         {
-            // Has a 2 dimensional array, containing the year and sale of wine
-            int[,] data =
+            // Creates an array, containing the sale of wine
+            int[] data =
             {
-                { 2009, 175134 },
-                { 2010, 175388 },
-                { 2011, 172818 },
-                { 2012, 142709 },
-                { 2013, 151437 },
-                { 2014, 152620 },
-                { 2015, 150979 },
-                { 2016, 152210 },
-                { 2017, 149450 },
-                { 2018, 154398 },
-                { 2019, 150160 }
+                175134, // 2009
+                175388, // 2010
+                172818, // 2011
+                142709, // 2012
+                151437, // 2013
+                152620, // 2014
+                150979, // 2015
+                152210, // 2016
+                149450, // 2017
+                154398, // 2018
+                150160 // 2019
             };
 
+            // Sends the array to wherever the method is being called from
             return data;
         }
     }
